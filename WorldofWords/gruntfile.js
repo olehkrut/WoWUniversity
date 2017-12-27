7502d9e478d37d841fc997f0f45f19e7b59851b7
olehkrut <olegkrutiy@gmail.com>
2017-12-27 21:26:20 +0200

    made static port

--- WorldofWords/gruntfile.js
+++ WorldofWords/gruntfile.js
@@ -20,19 +20,43 @@ module.exports = function (grunt) {
                 files: {
                     'dist/scripts-concat.js':
                         [
-                            "Scripts/angular/angular.js", "Scripts/angular-ui/ui-bootstrap.js", "Scripts/jquery/jquery-2.1.4.js", "Scripts/nicescroll.js", "Scripts/libs/angular-virtual-keyboard.js",
-                            "Scripts/libs/vki-layouts.js", "Scripts/libs/vki-deadkeys.js", "Scripts/libs/sha512.js", "Scripts/libs/Timer.js", 
+                        "Scripts/angular/angular.js",
+                        "Scripts/angular-ui/ui-bootstrap.js",
+                        "Scripts/jquery/jquery-2.1.4.js",
+                        "Scripts/nicescroll.js",
+                        "Scripts/libs/angular-virtual-keyboard.js",
+                        "Scripts/libs/vki-layouts.js",
+                        "Scripts/libs/vki-deadkeys.js",
+                        "Scripts/libs/sha512.js",
+                        "Scripts/libs/Timer.js",
+                        "Scripts/jquery.signalR-2.2.0.js",
+                        "Scripts/angular/angular-toastr.js",
+                        "Scripts/angular/angular-toastr.tpls.js",
+                        "Scripts/app/app.js",
+                        'Scripts/app/Controllers/*.js',
+                        "Scripts/app/Directives/*.js",
+                        "Scripts/app/Filters/*.js",
+                        "Scripts/app/Services/*.js",
+                        "Scripts/angular/angular-route.min.js",
+                        "Scripts/angular-ui/ui-bootstrap-tpls.min.js",
+                        "Scripts/jquery/jquery.scrollTo.min.js",
+                        "Scripts/libs/canvasjs.min.js",
 
-                            "Scripts/jquery.signalR-2.2.0.js", "Scripts/angular/angular-toastr.js", "Scripts/angular/angular-toastr.tpls.js",
-
-                            "Scripts/app/app.js", 'Scripts/app/Controllers/*.js', "Scripts/app/Directives/*.js", "Scripts/app/Filters/*.js", "Scripts/app/Services/*.js",
-
-                            "Scripts/angular/angular-route.min.js", "Scripts/angular-ui/ui-bootstrap-tpls.min.js", "Scripts/jquery/jquery.scrollTo.min.js", "Scripts/libs/canvasjs.min.js",
-                            "Scripts/libs/bootstrap.min.js", "Scripts/libs/lodash.min.js", "Scripts/isteven-multi-select.js", "Scripts/angular/angular-mocks.js"
+                        "Scripts/libs/bootstrap.min.js",
+                        "Scripts/libs/lodash.min.js",
+                        "Scripts/isteven-multi-select.js",
+                        "Scripts/angular/angular-mocks.js"
                         ],
 
                     'dist/styles-concat-dark-theme.css':
-                        ["Scripts/libs/angular-virtual-keyboard.css", "Content/drop-down-dark-theme.css", "Content/sidebar-dark-theme.css", "Content/bootstrap.min.css", "Content/bootstrap-theme.min.css", "Content/metro-bootstrap-dark-theme.min.css", "Content/angular-toastr.css", "Content/recorder.css"],
+                    ["Scripts/libs/angular-virtual-keyboard.css",
+                        "Content/drop-down-dark-theme.css",
+                        "Content/sidebar-dark-theme.css",
+                        "Content/bootstrap.min.css",
+                        "Content/bootstrap-theme.min.css",
+                        "Content/metro-bootstrap-dark-theme.min.css",
+                        "Content/angular-toastr.css",
+                        "Content/recorder.css"],
 
                     'dist/styles-concat-blue-theme.css':
                     ["Scripts/libs/angular-virtual-keyboard.css", "Content/drop-down-blue-theme.css", "Content/sidebar-blue-theme.css", "Content/bootstrap.min.css", "Content/bootstrap-theme.min.css", "Content/metro-bootstrap-blue-theme.min.css", "Content/angular-toastr.css", "Content/recorder.css", "Content/isteven-multi-select.css"],
